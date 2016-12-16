ec02ab9b9d4f711342f9fba6f84c984fdca0bc51
FabianS <fabian.seidler@hotmail.com>
2016-12-16 15:09:22 +0100

    Formular für Foodpornupload

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,8 +23,8 @@ loginApp.controller('FormController',
                     'Content-Type': 'application/x-www-form-urlencoded'
                 }
             });
-            console.log(document.getElementById("benutzer_login").value);
-            console.log(document.getElementById("passwort_login").value);
+            //console.log(document.getElementById("benutzer_login").value);
+            //console.log(document.getElementById("passwort_login").value);
             // Was kommt vom Controller.php zurück? Wenn passt, dann login. Wenn nicht dann redirect
             request.success(function(meldung) {
                 //Keine Rückmeldung von Server
@@ -97,8 +97,61 @@ mainApp.controller('buildMainEntrys',
     function mainController($scope, $http) {
         $http.get('./../php/Controller.php?class=foodporn').success(
             function(data) {
-                console.log(data);
+                //console.log(data);
                 $scope.entrys = data;
             }
         );
     });
+
+
+mainApp.controller('sendNewEntry',
+    function sendNewEntry($scope, $http) {
+        $scope.submit = function() {
+            // ------------------------------------------------------
+            // Formular - Datenübermitteln an controller.php
+            // ------------------------------------------------------ 
+            var request = $http({
+                method: "post",
+                url: './assets/php/controller.php?class=foodporn&action=add',
+                data: {
+                    titel_neuesBild: document.getElementById("titel_neuesBild").value,
+                    beschreibung_neuesBild: document.getElementById("beschreibung_neuesBild").value,
+                    kategorie_neuesBild: document.getElementById("kategorie_neuesBild").value,
+                    //datei_neuesBild: document.getElementById("datei_neuesBild").value
+                    datei_neuesBild: dataTrans()
+                },
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+            console.log(document.getElementById("datei_neuesBild").value);
+            console.log(dataTrans());
+            // Was kommt vom Controller.php zurück?
+            request.success(function(meldung) {
+                //Keine Rückmeldung von Server
+                if (meldung === null) {
+                    console.log("Fehlerhafte Rückmeldung von Server")
+                } else {
+                    if (meldung === 'true') {
+                        //Clear Inputs  
+                        var formModal = document.getElementById("formNewentryAdd");
+                        formModal.reset();
+                        //Hide Modal
+                        $('#meinModal').modal('hide');
+                    }
+                }
+            });
+        }
+    }
+);
+
+//Für Bildformatierung
+function dataTrans() {
+    var file = document.querySelector('#datei_neuesBild').files[0];
+    var reader = new FileReader();
+    if (file) {
+        var dataFile = reader.readAsDataURL(file);
+        console.log(dataFile);
+        return dataFile;
+    }
+}
